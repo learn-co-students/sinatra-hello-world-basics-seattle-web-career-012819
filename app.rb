0264af6a03512241249d5454d42d11aee14d0ce2
github username <github email address>
2019-02-16 23:47:06 -0800

    First commit. Resolved nokogiri bug.

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 class App < Sinatra::Base
-
-  # Define your GET '/' route below and respond with "Hello, World!"
-
-
+  # Create the #get method for the root path "/"
+  get '/' do
+    "Hello, World!"
+  end
 end
